27e9c6e49b46587e8e311a0616739e6d0c69bbb8
Sonnerz <Sonnerz@users.noreply.github.com>
2019-10-13 20:11:38 +0100

    Updates

---
+++ precache-manifest.8538b04d70413fe853cbcc0159fb84d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "486f879f9606aaedeeb4d7e572f8a3d2",
+    "url": "/Pomodoro-Clock/index.html"
+  },
+  {
+    "revision": "ad14cdaab74ca656a7d7",
+    "url": "/Pomodoro-Clock/static/css/main.ce337022.chunk.css"
+  },
+  {
+    "revision": "dc5bac670b0e6a843f1e",
+    "url": "/Pomodoro-Clock/static/js/2.ec818169.chunk.js"
+  },
+  {
+    "revision": "ad14cdaab74ca656a7d7",
+    "url": "/Pomodoro-Clock/static/js/main.59747f0e.chunk.js"
+  },
+  {
+    "revision": "4d727b3cb262c0552756",
+    "url": "/Pomodoro-Clock/static/js/runtime-main.df92c57d.js"
+  }
+]);
